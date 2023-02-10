735389e8ee717176b33fd3602edf04bccebf2195
unknown <J4sumit@gmail.com>
2023-02-10 11:36:20 +0530

    removeddupArrayele

---
+++ removedupArry.js
@@ -0,0 +1,10 @@
+function removedupArr()
+{
+var myArray = [10,15,10,8,8,16,24,84,18,8];
+var res = myArray.filter((item, index, arr) =>  arr.indexOf(item)== index);
+return res;
+}
+console.log(removedupArr());
+
+
+
