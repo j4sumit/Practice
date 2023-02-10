4c323c32e5d7c7e159f149927a3bdfd9b1d42071
unknown <J4sumit@gmail.com>
2023-02-10 12:16:37 +0530

    revert str with own logic

--- reverseStr.js
+++ reverseStr.js
@@ -1,7 +1,24 @@
 function reverseStr(){
     var str ="My name is Sumit Chaurasiya";
-    var res = str.split('').reverse().join('').split(' ').reverse().join(' ');
-    console.log(res);
+    // var res = str.split('').reverse().join('').split(' ').reverse().join(' ');
+
+    /*    var res = str.split('').reverse().join('');
+    console.log(res); */
+
+    let word ='';
+let res ='';
+for(let c of str){
+    if (c=== ' ')
+    {
+        res = res +word +c;
+        word ='';
+    }
+    else{
+        word =c +word;
+    }
+
+}
+return (res + word);
 }
 
-reverseStr();
+console.log(reverseStr());
