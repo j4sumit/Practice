e392e53f89c30bd058d3c0c51f9d41ccb04b823a
unknown <J4sumit@gmail.com>
2023-02-10 11:20:31 +0530

    removedupwithfilter

---
+++ removeduplicatechar.js
@@ -0,0 +1,8 @@
+function removeChar(){
+let Str = "Sumit Amit samir";
+let res = Str.split('').filter((item,index,arr) =>{ return arr.indexOf(item) == index; }).join('');
+return res;
+}
+
+console.log(removeChar());
+
