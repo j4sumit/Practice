52de2b494ebc106e218c7f733a5ec3ce8d50f9c0
unknown <J4sumit@gmail.com>
2023-02-13 10:57:45 +0530

    swaping without third number

--- swapping.js
+++ swapping.js
@@ -2,16 +2,23 @@ function swapping()
 {
     let a=10;
     let b= 20;
-    let c;
+    // let c;
     console.log("befor swappig");
     console.log("a is:- "+a);
     console.log("b is :- "+b);
-    c= a;
-    a=b;
-    b=c;
+    // c= a;
+    // a=b;
+    // b=c;
+    // console.log("After swappig");
+    // console.log("a is:- "+a);
+    // console.log("b is :- "+b);
+ a= a+b;  //30
+ b=a-b;    //10 
+ a=a-b;    //20 
     console.log("After swappig");
     console.log("a is:- "+a);
     console.log("b is :- "+b);
+ 
 
 }
 swapping();
