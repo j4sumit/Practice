0376fe01f319d04a300d96e88cde6a7d556fba04
unknown <J4sumit@gmail.com>
2023-02-15 11:52:24 +0530

    Anagram along with string

---
+++ Anagram.js
@@ -0,0 +1,16 @@
+function anagram()
+{
+var a ="Sumit kumar chaurasiya";
+var b ="sUMIT CHAURASIYA kuMAR"
+
+var a1 = a.toLowerCase().split('').sort().join().split(' ').reverse().join(' ');
+var b1 = b.toLowerCase().split('').sort().join().split(' ').reverse().join(' ');
+if(a1 == b1)
+{
+    console.log("Anagram");
+}
+else {
+    console.log("Not anagram");
+}
+}
+anagram();
