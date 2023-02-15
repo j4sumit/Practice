237ac914f6a3c5c58542da0b9bd4396af1924e00
unknown <J4sumit@gmail.com>
2023-02-15 10:30:07 +0530

    remove duplicate char or num

--- RemoveDup.js
+++ RemoveDup.js
@@ -11,6 +11,10 @@
 
 // removeStr();
 
-var arr = [55, 44, 55,67,67,67,67,8,8,8,8,8,65,1,2,3,3,34,5];
-var uniquue = [... new Set(arr)];
-console.log(uniquue);
+// var arr = [55, 44, 55,67,67,67,67,8,8,8,8,8,65,1,2,3,3,34,5];
+// var uniquue = [... new Set(arr)];
+// console.log(uniquue);
+
+var myvar = ['a',2, 4,2,4,8,5,8,'a','c','d','c','e','c','d','a','b'];
+var removed =myvar.filter((item, idex, arr) => {return arr.indexOf(item) == idex;});
+console.log(removed);
