c6cf17268f84a266adf313f885cd9b4e49b3baeb
unknown <J4sumit@gmail.com>
2023-02-15 10:24:29 +0530

    RemoveDup1.js

--- RemoveDup.js
+++ RemoveDup.js
@@ -1,12 +1,16 @@
-function removeStr(){
+// function removeStr(){
 
-var String1 = "Sumit uAmtitxyx";
-// var String1='priya riya supriya'
+// var String1 = "Sumit uAmtitxyx";
+// // var String1='priya riya supriya'
  
-var str = String1.split('').filter((item, index, array) => { return array.indexOf(item) == index; }).join('');
+// var str = String1.split('').filter((item, index, array) => { return array.indexOf(item) == index; }).join('');
 
 
-console.log(str);
-}
+// console.log(str);
+// }
 
-removeStr();
+// removeStr();
+
+var arr = [55, 44, 55,67,67,67,67,8,8,8,8,8,65,1,2,3,3,34,5];
+var uniquue = [... new Set(arr)];
+console.log(uniquue);
