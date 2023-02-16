01e49d0ea22223db93b35e3f278bab735710abfb
unknown <J4sumit@gmail.com>
2023-02-16 11:00:04 +0530

    largest word using method

--- smallestwordmethod.js
+++ smallestwordmethod.js
@@ -2,7 +2,8 @@ function smallestword()
 {
 var name = "my name is sumit chaurasiya i'm a front end developer";
 let smallestword = name.split(' ').sort((a,b) => b.length - a.length);
-return smallestword[smallestword.length-1];
+// return smallestword[smallestword.length-1];
+return smallestword[0];
 }
 
 console.log(smallestword());
