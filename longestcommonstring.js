b834ecc3c3eb6495e1d1c22fc92a317fd4960841
unknown <J4sumit@gmail.com>
2023-02-17 09:31:50 +0530

    longest common sting

---
+++ longestcommonstring.js
@@ -0,0 +1,13 @@
+function longestcommonstring(){
+    let array1 = ["Sunmit", "Sunressh", "Sunmit", "Sunkesh", "Sun"];
+    let word = array1.sort();
+    let i =0;
+while( word[0].length>0 && word[0].charAt(i)== word[word.length-1].charAt(i))
+{
+    i++;
+}
+// console.log(word[0].substring(0,i));
+return word[0].substring(0,i);
+}
+
+console.log(longestcommonstring());
