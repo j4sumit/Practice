0691f0e1076a867664609e6d5fbbe88dae4b72e3
unknown <J4sumit@gmail.com>
2023-02-18 19:55:55 +0530

    for every input two index to find the result of target

--- Twoindexfortarget.js
+++ Twoindexfortarget.js
@@ -1,12 +1,17 @@
 var twoSum = function(nums, target) {
     for( let i=0; i<nums.length; i++)
     {
-        if(nums[i]+nums[i+1]==target)
-        {
-            return [i, i+1];
+        // if(nums[i]+nums[i+1]==target)
+        // {
+        //     return [i, i+1];
+        // }
+        for (let j=0; j<nums.length; j++){
+            if(nums[i]+ nums[j]== target)
+            {
+                return [i,j];
+            }
         }
-    
     }
 };
 
-console.log(twoSum([2,7,11,15],9));
+console.log(twoSum([2,7,11,15],13));
