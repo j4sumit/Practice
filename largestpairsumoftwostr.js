9f626d2b034e79b6fe05557f58855bd52a1bc5f0
unknown <J4sumit@gmail.com>
2023-02-19 20:42:42 +0530

    largest pair sum of two array element

---
+++ largestpairsumoftwostr.js
@@ -0,0 +1,9 @@
+function largestpairsum(numbers)
+{
+let num =numbers.sort((a,b) => {return b - a})
+console.log(num);
+return num[0]+num[1];
+}
+
+const result = largestpairsum([9,7,8,4,5,6,1,2,3]);
+console.log(result);
