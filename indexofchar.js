fb48aec9c580e772afca54c245e455fcceacb983
unknown <J4sumit@gmail.com>
2023-02-19 20:58:15 +0530

    index of char

--- indexofchar.js
+++ indexofchar.js
@@ -1,6 +1,15 @@
 function indexofchar(arr, char)
 {
-return arr.indexOf(char);
+// return arr.indexOf(char);
+for(let i=0; i<arr.length; i++)
+{
+    if(arr[i]==char)
+    {
+        return i;
+    }
 }
 
-console.log(indexofchar(['a', 'b', 'c','d','e','f'], 'e'));
+
+}
+
+console.log(indexofchar(['a', 'b', 'c','d','e','f'], 'd'));
