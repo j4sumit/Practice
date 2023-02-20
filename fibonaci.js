3a172bb7c40559323872fb5de3092d6995525180
unknown <J4sumit@gmail.com>
2023-02-20 09:47:43 +0530

    fibonaci.js

--- fibonaci.js
+++ fibonaci.js
@@ -16,21 +16,42 @@
 // }
 
 // }
-function fibonaci ()
-{
-let limit = 5;
-	let n1 = 0; // 0,1,1,2,3,5,8
-	let n2 = 1;
-	let sum = 0;
+// function fibonaci ()
+// {
+// let limit = 5;
+// 	let n1 = 0; // 0,1,1,2,3,5,8
+// 	let n2 = 1;
+// 	let sum = 0;
 
-	for (var i = 0; i < limit; i++) {
-		console.log(n1);
-		n1  = n1+n2;
-         n2  = sum;
-        sum = n1;
+// 	for (var i = 0; i < limit; i++) {
+// 		console.log(n1);
+// 		n1  = n1+n2;
+//          n2  = sum;
+//         sum = n1;
 
         
 		
-	}
+// 	}
+// }
+// fibonaci();
+
+function fibonaci(Number)
+{
+	let n1 =0;
+	let n2 =1;
+	const arr =[];
+	let next =0;
+	arr.push(n1);
+	// arr.push(n2);
+for(let i=1; i<=Number; i++)
+{
+	console.log(n1);
+	next =n1+n2;
+	n1=n2;
+	n2=next;
+	arr.push(n1)
 }
-fibonaci();
+return arr;
+}
+
+console.log(fibonaci(8));
