61a5faec80e712b69ee107d5ec29953827bd2798
unknown <J4sumit@gmail.com>
2023-02-20 09:57:05 +0530

    fibonaci using first two elemet from  array

--- fibonaci.js
+++ fibonaci.js
@@ -35,23 +35,33 @@
 // }
 // fibonaci();
 
-function fibonaci(Number)
-{
-	let n1 =0;
-	let n2 =1;
-	const arr =[];
-	let next =0;
-	arr.push(n1);
-	// arr.push(n2);
-for(let i=1; i<=Number; i++)
-{
-	console.log(n1);
-	next =n1+n2;
-	n1=n2;
-	n2=next;
-	arr.push(n1)
-}
-return arr;
-}
+// function fibonaci(Number)
+// {
+// 	let n1 =0;
+// 	let n2 =1;
+// 	const arr =[];
+// 	let next =0;
+// 	arr.push(n1);
+// 	arr.push(n2);
+// for(let i=1; i<=Number; i++)
+// {
+// 	console.log(n1);
+// 	next =n1+n2;
+// 	n1=n2;
+// 	n2=next;
+// 	arr.push(next);
+// }
+// return arr;
+// }
 
-console.log(fibonaci(8));
+// console.log(fibonaci(8));
+
+
+function listFibonacci(n) {
+	var arr = [0, 1]
+	for (var  i = 0; i < n; i++) 
+    arr.push(arr[i] + arr[i + 1])
+
+  return arr
+ 	}
+	console.log(listFibonacci(4))
