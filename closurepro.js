dd2eacd61099e53695db4a259c191a4f372c9441
unknown <J4sumit@gmail.com>
2023-02-23 18:11:59 +0530

    closure

--- closurepro.js
+++ closurepro.js
@@ -1,11 +1,23 @@
+// function outer()
+// {
+//     var a = 10;
+//     function inner ()
+//     {
+//         console.log (`returing fron inner ${a}`);
+//     }
+//         return inner;
+// }
+
+// outer()();
+
 function outer()
 {
-    var a = 10;
-    function inner ()
-    {
-        console.log (`returing fron inner ${a}`);
+    var a= 25;
+    function inner(){
+        console.log(a);
     }
-        return inner;
+    return inner;
 }
 
-outer()();
+var result = outer();
+console.log(result());
