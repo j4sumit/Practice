3cfd56fb7a9322168355972839b4b159214af820
unknown <J4sumit@gmail.com>
2023-02-28 11:58:53 +0530

    String repeat methods

--- arraymethods.js
+++ arraymethods.js
@@ -1,5 +1,11 @@
-function confirmEnding(str,target){
-    return str.endsWith(target) //true
- }
- console.log(confirmEnding("priya","a"))
+// function confirmEnding(str,target){
+//     return str.endsWith(target) //true
+//  }
+//  console.log(confirmEnding("priya","a"))
  
+
+ function repeatStrinNumTimes(str, num){
+    if (num<1) return ""
+    return str.repeat(num)
+    }
+    console.log(repeatStrinNumTimes("Sumit ",3))
