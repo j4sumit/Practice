3e6236bc2092008f8b534737d97205bee1a4d0e6
unknown <J4sumit@gmail.com>
2023-02-28 11:51:03 +0530

    Find lagest number in twoD and and store in another array

--- largestnointwod.js
+++ largestnointwod.js
@@ -1,16 +1,31 @@
+// function largestFromArray(arr){
+//     var max=[];
+//     for(var i=0; i<arr.length;i++){
+//       var tempMax =arr[i][0] 
+//       for(var j=0; j<arr[i].length; j++){
+//          var currElement = arr[i][j];
+//          if(currElement>=tempMax){
+//            tempMax = currElement
+//          }
+//       }
+//        max.push(tempMax)
+//     }
+//    console.log(max)
+//     return max;
+//  }
+//  largestFromArray([[1,2,3,4],[5,6,7,9],[45,76,2,1],[89,90,87,9]]);
+
 function largestFromArray(arr){
-    var max=[];
-    for(var i=0; i<arr.length;i++){
-      var tempMax =arr[i][0] 
-      for(var j=0; j<arr[i].length; j++){
-         var currElement = arr[i][j];
-         if(currElement>=tempMax){
-           tempMax = currElement
-         }
+   var max=[0,0,0,0];
+   for(var i=0; i<arr.length;i++){
+      for(var j=0; j<arr[i].length; j++)
+      {
+          if(arr[i][j]>=max[i]){
+          max[i] = arr[i][j]
+        }
       }
-       max.push(tempMax)
-    }
-   console.log(max)
-    return max;
- }
- largestFromArray([[1,2,3,4],[5,6,7,9],[45,76,2,1],[89,90,87,9]]);
+   }
+  console.log(max)
+  return max;
+}
+largestFromArray([[1,2,3,4],[5,6,7,9],[45,76,2,1],[89,90,87,9]])
