0b685fd71a8f74205333e8781acbed00337c2c90
unknown <J4sumit@gmail.com>
2023-03-01 11:56:16 +0530

    Print string n times in custom way using recursion

--- arraymethods.js
+++ arraymethods.js
@@ -12,15 +12,25 @@
 
 
     // Print string n times in custom way
-    function repeatStr(str, num){
-if(num<=0) 
-return "kindly share exact number of element";
-var res ="";
-for(let i=0;i<num;i++)
-{
-   res = str + res;
-}
- return res;
-  }
+//     function repeatStr(str, num){
+// if(num<=0) 
+// return "kindly share exact number of element";
+// var res ="";
+// for(let i=0;i<num;i++)
+// {
+//    res = str + res;
+// }
+//  return res;
+//   }
 
-  console.log(repeatStr("Sumit ", 6));
+//   console.log(repeatStr("Sumit ", 6));
+
+
+function repeatStr(str, num){
+   if(num<=0) 
+   return "kindly share exact number of element";
+if(num==1) return str;
+return str+ repeatStr(str, num-1)
+}
+   console.log(repeatStr("Sumit ", 4));
+   
