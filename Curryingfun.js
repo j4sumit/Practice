66f754578595aa7686cacbf632fdbf6c9301504c
unknown <J4sumit@gmail.com>
2023-03-04 12:18:49 +0530

    Currying function

---
+++ Curryingfun.js
@@ -0,0 +1,14 @@
+function Sum(a)
+{
+    return function(b)
+    {
+        return function(c)
+        {
+            
+                return a+b+c;
+                 
+            }
+    }
+}
+
+console.log(Sum(2)(12)(8));
