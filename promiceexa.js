8ea84f841eafa80e78af437925250cf64469f067
unknown <J4sumit@gmail.com>
2023-03-07 22:08:24 +0530

    function using promice

--- promiceexa.js
+++ promiceexa.js
@@ -1,13 +1,30 @@
-let myPromice = new Promise(function(resolve, reject){
-let num =10;
-if(num%2==0)
-{
-    resolve("number is Even");
-}
-else
-{
-    reject("number  is odd");
-}
-});
+// let myPromice = new Promise(function(resolve, reject){
+// let num =10;
+// if(num%2==0)
+// {
+//     resolve("number is Even");
+// }
+// else
+// {
+//     reject("number  is odd");
+// }
+// });
 
-myPromice.then(function(mes){console.log(mes)}).catch(function(mes){console.log(mes);});
+// myPromice.then(function(mes){console.log(mes)}).catch(function(mes){console.log(mes);});
+
+function isevenodd(num)
+{
+    return new Promise(function(resolve, reject){
+        if(num%2==0)
+        {
+            resolve("number is Even");
+        }
+        else
+        {
+            reject("number  is odd");
+        }
+        });
+        
+}
+
+isevenodd(5).then(function(ans){console.log(ans);}).catch(function(ans){console.log(ans);});
