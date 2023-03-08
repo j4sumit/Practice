7d459ef5e5d5f48fc1e98c54c9b89284eb7c695d
unknown <J4sumit@gmail.com>
2023-03-08 23:31:03 +0530

    removing particular element from given array

---
+++ Remove.js
@@ -0,0 +1,11 @@
+function Remove(arr, n)
+{
+
+       for( var i = 0; i < arr.length; i++){ 
+           if ( arr[i] === n) { 
+              arr.splice(i, 1); 
+           }
+       }
+       return arr;
+    }
+console.log(Remove([1, 2, 3, 4, 5, 6, 7, 8, 9, 0],5));
