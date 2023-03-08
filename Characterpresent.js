4c552af2cedda935f923de56182c16549a2ef437
unknown <J4sumit@gmail.com>
2023-03-08 23:16:29 +0530

    checking second str  present in first str or not using indexOf method

--- Characterpresent.js
+++ Characterpresent.js
@@ -1,26 +1,26 @@
-// function characterPresent(arr){
-//     var first = arr[0].toLowerCase()
-//     var second = arr[1].toLowerCase()
-//     for (var letter of second){
-//       if(!first.includes(letter)){
-//         return false
-//       }
-//     }
-//     return true
-//   }
-//   console.log(characterPresent(["hello","hey"]))
-
-function Characterpresent(arr)
-{
-let first = arr[0].toLowerCase();
-let second =arr[1].toLowerCase();
-for(let str of second)
-{
-    if(!first.includes(str)){
-        return false;
+function characterPresent(arr){
+    var first = arr[0].toLowerCase()
+    var second = arr[1].toLowerCase()
+    for (var letter of second){
+      if(first.indexOf(letter)== -1){
+        return false
+      }
     }
-}
-return true;
-}
+    return true
+  }
+  console.log(characterPresent(["hello","hel"]))
 
-console.log(Characterpresent(['hello', 'hel']));
+// function Characterpresent(arr)
+// {
+// let first = arr[0].toLowerCase();
+// let second =arr[1].toLowerCase();
+// for(let str of second)
+// {
+//     if(!first.includes(str)){
+//         return false;
+//     }
+// }
+// return true;
+// }
+
+// console.log(Characterpresent(['hello', 'hel']));
