4d28279dc0139d6ada24c996f33c5cb829fb61fd
unknown <J4sumit@gmail.com>
2023-03-08 23:37:50 +0530

    removing element  using filter methode

--- Remove.js
+++ Remove.js
@@ -1,11 +1,19 @@
+// function Remove(arr, n)
+// {
+
+//        for( var i = 0; i < arr.length; i++){ 
+//            if ( arr[i] === n) { 
+//               arr.splice(i, 1); 
+//            }
+//        }
+//        return arr;
+//     }
+// console.log(Remove([1, 2, 3, 4, 5, 6, 7, 8, 9, 0],5));
+
 function Remove(arr, n)
 {
 
-       for( var i = 0; i < arr.length; i++){ 
-           if ( arr[i] === n) { 
-              arr.splice(i, 1); 
-           }
-       }
-       return arr;
+    return arr.filter(ele => ele!== n);
+
     }
-console.log(Remove([1, 2, 3, 4, 5, 6, 7, 8, 9, 0],5));
+console.log(Remove([1, 2, 3, 4, 5, 6, 7, 8, 9, 0],8));
