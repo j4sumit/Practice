f28065d296dbda88e49faef9a86c2a03ae5dc8b6
unknown <J4sumit@gmail.com>
2023-03-09 23:59:15 +0530

    deleting arrayelement using length property

--- sumfromstarttoend.js
+++ sumfromstarttoend.js
@@ -1,10 +1,14 @@
-function sumFromStartToEnd(arr){
-    var start = Math.min(arr[0], arr[1])
-    var end = Math.max(arr[0], arr[1])
-    sum =0
-    for(var i= start; i<=end; i++){
-      sum+=i
-    }
-    return sum
-  }
-  console.log(sumFromStartToEnd([1,4]))
+// function sumFromStartToEnd(arr){
+//     var start = Math.min(arr[0], arr[1])
+//     var end = Math.max(arr[0], arr[1])
+//     sum =0
+//     for(var i= start; i<=end; i++){
+//       sum+=i
+//     }
+//     return sum
+//   }
+//   console.log(sumFromStartToEnd([1,4]));
+
+var ar = [1, 2, 3, 4, 5, 6]; 
+       ar.length = 4; 
+       console.log( ar);
