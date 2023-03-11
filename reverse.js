2ecf53d56195e3c40850b32d6b3e68894094f3ba
unknown <J4sumit@gmail.com>
2023-03-11 23:28:24 +0530

    revert number with the help of Math.floor()

--- reverse.js
+++ reverse.js
@@ -1,9 +1,25 @@
-function reverse()
+// function reverse()
+// {
+//     var string = "My name is Sumit Chaurasiya";
+//     // var str1 =string.split('').reverse().join('').split(' ').reverse().join(' ');
+//     var str1 =string.split('').reverse().join('');
+//     return str1;
+// }
+
+// console.log(reverse());
+
+
+
+
+function reverse(num)
 {
-    var string = "My name is Sumit Chaurasiya";
-    // var str1 =string.split('').reverse().join('').split(' ').reverse().join(' ');
-    var str1 =string.split('').reverse().join('');
-    return str1;
+    let res=0;
+    while(num>0)
+    {
+res = res*10 + num%10;
+num = Math.floor(num/10);
+    }
+return res;
 }
 
-console.log(reverse());
+console.log(reverse(12345643));
