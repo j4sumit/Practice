0fffc6c4fce5487ca80fcdf29d5a919531dc23b5
unknown <J4sumit@gmail.com>
2023-03-13 23:44:27 +0530

    Alternate character has been changed in uppercase

---
+++ Alternatechar.js
@@ -0,0 +1,11 @@
+function Alternatechartoupper(str)
+{
+let str1 = str.toLowerCase().split('');
+for(let i=0; i<str1.length; i=i+2)
+{
+    str1[i] = str1[i].toUpperCase();
+}
+return str1.join('');
+}
+
+console.log(Alternatechartoupper("Sumit Chaurasiya"));
