e27713b18e5dedf1ecaa5d7ce1ebe2ba67211041
unknown <J4sumit@gmail.com>
2023-03-16 22:37:58 +0530

    Missing array element

--- Missingarrayitem.js
+++ Missingarrayitem.js
@@ -1,20 +1,37 @@
-function missingarrayitem()
+// function missingarrayitem()
+// {
+// let arr = [1, 3, 6,7,8,15,16];
+
+// let missingSeries = [];
+
+// for (let i = 0; i < arr.length; i++) 
+// { if (arr[i + 1] - arr[i] !== 1) 
+//     { let j = arr[i] + 1; 
+//         while (j < arr[i + 1]) 
+//         { 
+//             missingSeries.push(j); 
+//             j++; 
+//         } 
+//     } 
+// } 
+// return missingSeries;
+// }
+
+// console.log(missingarrayitem());
+
+
+function MissingElements(arr)
 {
-let arr = [1, 3, 6,7,8,15,16];
-
-let missingSeries = [];
-
-for (let i = 0; i < arr.length; i++) 
-{ if (arr[i + 1] - arr[i] !== 1) 
-    { let j = arr[i] + 1; 
-        while (j < arr[i + 1]) 
-        { 
-            missingSeries.push(j); 
-            j++; 
-        } 
-    } 
-} 
-return missingSeries;
+    for(let i = 0; i < arr.length; i++)
+    {
+        if (arr[0] != arr[i] - i) 
+        {
+            while (arr[0] < arr[i] - i)
+            {
+                console.log(arr[0]+i);
+                arr[0]++; 
+            }
+        }
+    }
 }
-
-console.log(missingarrayitem());
+MissingElements([1,2,6])
