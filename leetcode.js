8a4766062267a23a95b990a878bace1ddf7873cb
unknown <J4sumit@gmail.com>
2023-03-17 22:16:14 +0530

    leetcode Number of Good Pairs

--- leetcode.js
+++ leetcode.js
@@ -41,20 +41,43 @@
 // Explanation: The array leftSum is [0,10,14,22] and the array rightSum is [15,11,3,0].
 // The array answer is [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22].
 
-function leftrightSumdiff(nums) {
-    let len = nums.length;
+// function leftrightSumdiff(nums) {
+//     let len = nums.length;
  
-     let leftSum = [0];
-     let rightSum = [0];
-     for (let i = 0; i < len - 1; i++) {
-         leftSum.push(nums[i] + leftSum[leftSum.length - 1]);
-         rightSum.push(nums[len - 1 - i] + rightSum[rightSum.length - 1]);
+//      let leftSum = [0];
+//      let rightSum = [0];
+//      for (let i = 0; i < len - 1; i++) {
+//          leftSum.push(nums[i] + leftSum[leftSum.length - 1]);
+//          rightSum.push(nums[len - 1 - i] + rightSum[rightSum.length - 1]);
+//         }
+ 
+//      let res = [];
+//      for (let i = 0; i < len; i++) {
+//          res.push(Math.abs(leftSum[i] - rightSum[len - 1 - i]));
+//      }
+//      return res;
+//  };
+// console.log(leftrightSumdiff([10,4,8,3]));
+
+// 1512. Number of Good Pairs
+// A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+// Input: nums = [1,2,3,1,1,3]
+// Output: 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+function Goodpairs(nums) {
+    let count=0;
+    for(let i=0; i<nums.length;i++)
+    {
+        for(let j=1;j<nums.length;j++)
+        {
+            if(nums[i]==nums[j] & (i<j))
+            {
+                count=count+1;
+            }
         }
- 
-     let res = [];
-     for (let i = 0; i < len; i++) {
-         res.push(Math.abs(leftSum[i] - rightSum[len - 1 - i]));
-     }
-     return res;
- };
-console.log(leftrightSumdiff([10,4,8,3]));
+    }
+    return count;
+};
+
+console.log(Goodpairs([1,2,3,1,1,3]));
