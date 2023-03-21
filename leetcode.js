d22b2b4a67ae29ead625045f82de58a36267a1ab
unknown <J4sumit@gmail.com>
2023-03-21 18:45:01 +0530

    leetcode Count Number of Pairs With Absolute Difference K

--- leetcode.js
+++ leetcode.js
@@ -355,16 +355,43 @@
 // Input: s = "Hello how are you Contestant", k = 4
 // Output: "Hello how are you"
 
-function truncateSentence (s, k) {
-  let arr = s.split(" ");
-  let newarr =[];
-  for(let i=0;i<k; i++)
-  {
-    newarr.push(arr[i]);
-  } 
+// function truncateSentence (s, k) {
+//   let arr = s.split(" ");
+//   let newarr =[];
+//   for(let i=0;i<k; i++)
+//   {
+//     newarr.push(arr[i]);
+//   } 
 
-return newarr.join(" ");
+// return newarr.join(" ");
 
-}
+// }
  
-console.log(truncateSentence("Hello how are you Contestant", 4));
+// console.log(truncateSentence("Hello how are you Contestant", 4));
+
+// 2006. Count Number of Pairs With Absolute Difference K
+// Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+
+// The value of |x| is defined as:
+
+// x if x >= 0.
+// -x if x < 0.
+// Input: nums = [1,2,2,1], k = 1
+// Output: 4 
+
+function countKDifference(nums, k) {
+  let count =0;
+  for(let i=0; i<nums.length; i++)
+  {
+      for(let j=0; j<nums.length; j++)
+      {
+          if(nums[i]-nums[j]==k)
+          {
+              count++;
+          }
+      }
+  } 
+  return count;
+};
+
+console.log(countKDifference([1,2,2,1],1));
