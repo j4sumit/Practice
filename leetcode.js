a836349c0f8fd8646ed23461b53c95f47efee401
unknown <J4sumit@gmail.com>
2023-03-25 00:20:57 +0530

    leetcoed 2586. Count the Number of Vowel Strings in Range

--- leetcode.js
+++ leetcode.js
@@ -680,27 +680,65 @@
 // Explanation: The first string that is palindromic is "ada".
 // Note that "racecar" is also palindromic, but it is not the first.
 
-var firstPalindrome = function(words) {
+// var firstPalindrome = function(words) {
 
 
+//     let word;
+//         for(let j=0; j<words.length; j++)
+//         {
+//             word= words[j];
+//             let tempword=""; 
+//     for(let i=word.length-1; i>=0; i--)
+//        {
+//     tempword= tempword+ word[i];
+//        }
+
+    
+//          if(word=== tempword)
+//             {
+//                 return word;
+//             }
+    
+//         }
+//         return "";
+//     };
+
+//     console.log(firstPalindrome(["abc","car","ada","racecar","cool"]));
+
+
+// 2586. Count the Number of Vowel Strings in Range
+// You are given a 0-indexed array of string words and two integers left and right.
+
+// A string is called a vowel string if it starts with a vowel character and ends with a vowel character where vowel characters are 'a', 'e', 'i', 'o', and 'u'.
+
+// Return the number of vowel strings words[i] where i belongs to the inclusive range [left, right].
+
+// Input: words = ["are","amy","u"], left = 0, right = 2
+// Output: 2
+// Explanation: 
+// - "are" is a vowel string because it starts with 'a' and ends with 'e'.
+// - "amy" is not a vowel string because it does not end with a vowel.
+// - "u" is a vowel string because it starts with 'u' and ends with 'u'.
+// The number of vowel strings in the mentioned range is 2.
+
+var vowelStrings = function(words, left, right) {
+    let vowels=['a','e','i','o','u'];
+    let count =0;
     let word;
-        for(let j=0; j<words.length; j++)
-        {
-            word= words[j];
-            let tempword=""; 
-    for(let i=word.length-1; i>=0; i--)
-       {
-    tempword= tempword+ word[i];
-       }
-
+    for(let i=0; i<words.length; i++)
+    {
+        word =words[i].toLowerCase();
+        console.log(vowels.includes(word[0]));
+        console.log(vowels.includes(word[word.length-1]));
+        
+if(vowels.includes(word[0]) && vowels.includes(word[word.length-1]) )
+{
+        count++;
     
-         if(word=== tempword)
-            {
-                return word;
-            }
-    
-        }
-        return "";
-    };
+}
+    }
+    return count;
 
-    console.log(firstPalindrome(["abc","car","ada","racecar","cool"]));
+};
+
+console.log(vowelStrings(["are","amy","u"],0,2))
