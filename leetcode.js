d37eb05934f09bd98bbee5c39fab7cf604c4f995
unknown <J4sumit@gmail.com>
2023-03-24 23:18:49 +0530

    leetcode 2553. Separate the Digits in an Array

--- leetcode.js
+++ leetcode.js
@@ -615,26 +615,58 @@
 // - nums[2] == nums[4], and 2 * 4 == 8, which is divisible by 2.
 // - nums[3] == nums[4], and 3 * 4 == 12, which is divisible by 2.
 
-var countPairs = function(nums, k) {
-    let indexes;
-    let count =0;
+// var countPairs = function(nums, k) {
+//     let indexes;
+//     let count =0;
     
-    for(let i=0;i<nums.length; i++)
-    {
-        for(let j=i+1;j<nums.length; j++)
-        {
-            if(nums[i]===nums[j])
-            {
-indexes = i*j;
-{ 
-    if(indexes%k ===0)
-    {
-        count++;
-    }
-}
-            }
-        }
-    }
-    return count;
-};
-console.log(countPairs([3,1,2,2,2,1,3],2));
+//     for(let i=0;i<nums.length; i++)
+//     {
+//         for(let j=i+1;j<nums.length; j++)
+//         {
+//             if(nums[i]===nums[j])
+//             {
+// indexes = i*j;
+// { 
+//     if(indexes%k ===0)
+//     {
+//         count++;
+//     }
+// }
+//             }
+//         }
+//     }
+//     return count;
+// };
+// console.log(countPairs([3,1,2,2,2,1,3],2));
+
+// 2553. Separate the Digits in an Array
+// Given an array of positive integers nums, return an array answer that consists of the digits of each integer in nums after separating them in the same order they appear in nums.
+
+// To separate the digits of an integer is to get all the digits it has in the same order.
+
+// For example, for the integer 10921, the separation of its digits is [1,0,9,2,1].
+// Input: nums = [13,25,83,77]
+// Output: [1,3,2,5,8,3,7,7]
+// Explanation: 
+// - The separation of 13 is [1,3].
+// - The separation of 25 is [2,5].
+// - The separation of 83 is [8,3].
+// - The separation of 77 is [7,7].
+// answer = [1,3,2,5,8,3,7,7]. Note that answer contains the separations in the same order.
+
+var separateDigits = function(nums) {
+    let res = [];
+//  return nums.join("").split("");
+ for(let i=0; i<nums.length; i++)
+ {
+ str = nums[i]+"";
+ for(let j=0; j<str.length; j++)
+ {
+     res.push(Number(str[j]));
+ }
+ }
+ return res;
+ };
+
+
+ console.log(separateDigits([13,25,83,77]));
