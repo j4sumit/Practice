191ca2dd4233c37a66037ce32274b023d7c03288
unknown <J4sumit@gmail.com>
2023-04-11 21:54:21 +0530

    1295. Find Numbers with Even Number of Digits

--- leetcode.js
+++ leetcode.js
@@ -721,25 +721,53 @@
 // - "u" is a vowel string because it starts with 'u' and ends with 'u'.
 // The number of vowel strings in the mentioned range is 2.
 
-var vowelStrings = function(words, left, right) {
-    let vowels=['a','e','i','o','u'];
-    let count =0;
-    let word;
-    // for(let i=0; i<words.length; i++)
-    for(let i=left; i<=right; i++)
-    {
-        word =words[i].toLowerCase();
-        // console.log(vowels.includes(word[0]));
-        // console.log(vowels.includes(word[word.length-1]));
+// var vowelStrings = function(words, left, right) {
+//     let vowels=['a','e','i','o','u'];
+//     let count =0;
+//     let word;
+//     // for(let i=0; i<words.length; i++)
+//     for(let i=left; i<=right; i++)
+//     {
+//         word =words[i].toLowerCase();
+//         // console.log(vowels.includes(word[0]));
+//         // console.log(vowels.includes(word[word.length-1]));
         
-if(vowels.includes(word[0]) && vowels.includes(word[word.length-1]) )
-{
-        count++;
+// if(vowels.includes(word[0]) && vowels.includes(word[word.length-1]) )
+// {
+//         count++;
     
-}
+// }
+//     }
+//     return count;
+
+// };
+
+// console.log(vowelStrings(["are","amy","u"],0,2))
+
+// 1295. Find Numbers with Even Number of Digits
+// Given an array nums of integers, return how many of them contain an even number of digits.
+// Input: nums = [12,345,2,6,7896]
+// Output: 2
+// Explanation: 
+// 12 contains 2 digits (even number of digits). 
+// 345 contains 3 digits (odd number of digits). 
+// 2 contains 1 digit (odd number of digits). 
+// 6 contains 1 digit (odd number of digits). 
+// 7896 contains 4 digits (even number of digits). 
+// Therefore only 12 and 7896 contain an even number of digits.
+
+var findNumbers = function(nums) {
+    let count =0;
+    for(let i=0; i<nums.length; i++)
+    {
+        let digit =nums[i].toString().length;
+        if(digit%2==0)
+        {
+            count++;
+        }
     }
     return count;
+  
+  }
 
-};
-
-console.log(vowelStrings(["are","amy","u"],0,2))
+console.log(findNumbers([12,345,2,6,7896]));
