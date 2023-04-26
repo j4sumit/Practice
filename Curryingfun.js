d3f35f523cba27b21465bea4a5eae4ed8bf969c0
unknown <J4sumit@gmail.com>
2023-04-26 18:40:21 +0530

    Repractice Currying

--- Curryingfun.js
+++ Curryingfun.js
@@ -1,14 +1,27 @@
-function Sum(a)
+// function Sum(a)
+// {
+//     return function(b)
+//     {
+//         return function(c)
+//         {
+            
+//                 return a+b+c;
+                 
+//             }
+//     }
+// }
+
+// console.log(Sum(2)(12)(8));
+
+function mul(a)
 {
     return function(b)
     {
         return function(c)
         {
-            
-                return a+b+c;
-                 
-            }
+            return a*b*c;
+        }
     }
 }
 
-console.log(Sum(2)(12)(8));
+console.log(mul(2)(3)(5));
