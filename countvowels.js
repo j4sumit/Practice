5b7566dafe2095e71245c579b5745198373c2817
unknown <J4sumit@gmail.com>
2023-04-26 18:36:48 +0530

    repractice

--- countvowels.js
+++ countvowels.js
@@ -1,19 +1,34 @@
-function vowels()
-{
-    let vowels = ['a','e','i','o','u'];
-    let string = "sumitaaaUUUU";
-    let count=0;
+// function vowels()
+// {
+//     let vowels = ['a','e','i','o','u'];
+//     let string = "sumitaaaUUUU";
+//     let count=0;
 
-for (let i of string.toLowerCase())
-{
-    if (vowels.includes(i))
-    {
-        count++;
-        console.log(i);
+// for (let i of string.toLowerCase())
+// {
+//     if (vowels.includes(i))
+//     {
+//         count++;
+//         console.log(i);
         
-    }
-} 
-return count;
-}
+//     }
+// } 
+// return count;
+// }
 
-console.log(vowels());
+// console.log(vowels());
+
+function countv(str)
+{
+ let vowels =['a','e','i','o','u'];
+ let count=0;
+ for(let i=0; i<str.length; i++)
+ {
+     if(vowels.includes(str[i]))
+     {
+         count++;
+     }
+ }
+console.log(count);
+}
+countv("Sumit kumar chaurasiya");
