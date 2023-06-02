104a2c2c467382f67dd205b5645f7582349320ef
unknown <J4sumit@gmail.com>
2023-06-03 01:08:39 +0530

    Return the lowest index at which a value should be inserted

--- leetcode.js
+++ leetcode.js
@@ -1625,17 +1625,36 @@
 
 // Hint: Try converting each value to a Boolean.
 
-function bouncer(arr) {
-    const res=[];
-    for(let i=0; i<arr.length; i++)
-    {
-      if(arr[i])
-      {
-        res.push(arr[i])
-      }
-    }
-    return res;
-  }
+// function bouncer(arr) {
+//     const res=[];
+//     for(let i=0; i<arr.length; i++)
+//     {
+//       if(arr[i])
+//       {
+//         res.push(arr[i])
+//       }
+//     }
+//     return res;
+//   }
   
 
-  console.log(  bouncer([7, "ate", "", false, 9]));
+//   console.log(  bouncer([7, "ate", "", false, 9]));
+
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+// For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+// Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+
+function getIndexToIns(arr, num) {
+    let match = arr.sort((a,b)=> a-b).find((item)=> item>=num);
+    if(match)
+    {
+  let indexofitem= arr.indexOf(match);
+    return indexofitem;
+    }
+    else{
+      return arr.length;
+    }
+  }
+  console.log(getIndexToIns([10, 20, 30, 40, 50], 30));
