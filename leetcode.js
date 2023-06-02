142af0768e05e50d9e672ad25d755c06c60fcd92
unknown <J4sumit@gmail.com>
2023-06-02 21:06:07 +0530

    returned trucated string with ...

--- leetcode.js
+++ leetcode.js
@@ -1529,18 +1529,40 @@
 
 
 
-function repeatStringNumTimes(str, num) {
-    let res="";
-    if(num<=0)
-    {
-      res=""
-    }
-    else{
-      for(let i=1; i<=num; i++)
-      {
-   res = res +str;
+// function repeatStringNumTimes(str, num) {
+//     let res="";
+//     if(num<=0)
+//     {
+//       res=""
+//     }
+//     else{
+//       for(let i=1; i<=num; i++)
+//       {
+//    res = res +str;
+//       }
+//     }
+//      return res;
+//    }
+//    console.log(repeatStringNumTimes("abc", 3));
+
+
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+
+
+
+function truncateString(str, num) {
+    let res = "";
+    let len = str.length;
+    if (len > num) {
+      for (let i = 0; i < num; i++) {
+        res = res + str[i];
       }
+      res = res + "...";
+    } else {
+      res = str;
     }
-     return res;
-   }
-   console.log(repeatStringNumTimes("abc", 3));
+  
+    return res;
+  }
+  console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+  
