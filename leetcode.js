5050248cb98b65b641ed5a849a6b19e10dec4297
unknown <J4sumit@gmail.com>
2023-06-02 22:52:53 +0530

    check type of parameter in boolean permitive

--- leetcode.js
+++ leetcode.js
@@ -1569,10 +1569,26 @@
 //Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
 
 
-function findElement(arr, func) {
-    let num = 0;
-    num= arr.find(func);
-    return num;
-  }
+// function findElement(arr, func) {
+//     let num = 0;
+//     num= arr.find(func);
+//     return num;
+//   }
   
-  console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
+//   console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
+
+// Check if a value is classified as a boolean primitive. Return true or false.
+
+// Boolean primitives are true and false.
+
+function booWho(bool) {
+  
+    if(typeof(bool) === "boolean")
+    {
+  return true;
+    }
+    else{
+      return false;
+    }
+  }
+  console.log(booWho(null));
