e44ebb5fb789dca4530ec380eb3cabbd1e983254
unknown <J4sumit@gmail.com>
2023-06-02 22:41:18 +0530

    return first matched element

--- leetcode.js
+++ leetcode.js
@@ -1550,19 +1550,29 @@
 
 
 
-function truncateString(str, num) {
-    let res = "";
-    let len = str.length;
-    if (len > num) {
-      for (let i = 0; i < num; i++) {
-        res = res + str[i];
-      }
-      res = res + "...";
-    } else {
-      res = str;
-    }
+// function truncateString(str, num) {
+//     let res = "";
+//     let len = str.length;
+//     if (len > num) {
+//       for (let i = 0; i < num; i++) {
+//         res = res + str[i];
+//       }
+//       res = res + "...";
+//     } else {
+//       res = str;
+//     }
   
-    return res;
+//     return res;
+//   }
+//   console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+  
+//Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+
+
+function findElement(arr, func) {
+    let num = 0;
+    num= arr.find(func);
+    return num;
   }
-  console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
   
+  console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
