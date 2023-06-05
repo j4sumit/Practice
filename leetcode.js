76217761dda0d3bb5db2720861e38e119518e96d
Sumit Kumar <j4sumit@gmail.com>
2023-06-05 11:50:06 +0530

    Functional Programming Terminology

--- leetcode.js
+++ leetcode.js
@@ -1938,15 +1938,34 @@
 
 //Learn About Functional Programming
 
-const prepareTea = () => 'greenTea';
-const getTea = (numOfCups) => {
-  const teaCups = [];
+// const prepareTea = () => 'greenTea';
+// const getTea = (numOfCups) => {
+//   const teaCups = [];
 
+//   for(let cups = 1; cups <= numOfCups; cups += 1) {
+//     const teaCup = prepareTea();
+//     teaCups.push(teaCup);
+//   }
+//   return teaCups;
+// };
+// const tea4TeamFCC = getTea(40);
+// console.log(tea4TeamFCC);
+
+
+
+const prepareGreenTea = () => 'greenTea';
+const prepareBlackTea = () => 'blackTea';
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
   for(let cups = 1; cups <= numOfCups; cups += 1) {
     const teaCup = prepareTea();
     teaCups.push(teaCup);
   }
   return teaCups;
 };
-const tea4TeamFCC = getTea(40);
-console.log(tea4TeamFCC);
+const tea4GreenTeamFCC = getTea(prepareGreenTea,27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea,13);
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
