d70626bb977a4f2dbe12c17eabbc9bd1571a06f3
Sumit Kumar <j4sumit@gmail.com>
2023-06-05 22:10:49 +0530

    Use of Every method()

--- leetcode.js
+++ leetcode.js
@@ -2410,7 +2410,16 @@
 
 // Apply Functional Programming to Convert Strings to URL Slugs
 
-function urlSlug(title) {
-  return title.toLowerCase().trim().split(/\s+/).join("-");
-  }
-  console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
+// function urlSlug(title) {
+//   return title.toLowerCase().trim().split(/\s+/).join("-");
+//   }
+//   console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
+
+
+// Use the every Method to Check that Every Element in an Array Meets a Criteria
+
+function checkPositive(arr) {
+return arr.every((item)=> item>0)
+}
+
+checkPositive([1, 2, 3, -4, 5]);
