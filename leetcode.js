fb4696ca28eec919051566dc0409d93a3e320492
Sumit Kumar <j4sumit@gmail.com>
2023-06-05 10:24:34 +0530

    IIFE function

--- leetcode.js
+++ leetcode.js
@@ -1914,18 +1914,24 @@
 // A constructor function that inherits its prototype object from a supertype constructor function can still have its own methods in addition to inherited methods.
 
 
-function Animal() { }
-Animal.prototype.eat = function() { console.log("nom nom nom"); };
+// function Animal() { }
+// Animal.prototype.eat = function() { console.log("nom nom nom"); };
 
-function Dog() { }
-Dog.prototype = Object.create(Animal.prototype);
-Dog.prototype.constructor = Dog
+// function Dog() { }
+// Dog.prototype = Object.create(Animal.prototype);
+// Dog.prototype.constructor = Dog
 
-Dog.prototype.bark = function(){
-  console.log("Woof!");
-}
+// Dog.prototype.bark = function(){
+//   console.log("Woof!");
+// }
 
-let beagle = new Dog();
+// let beagle = new Dog();
 
-beagle.eat();
-beagle.bark();
+// beagle.eat();
+// beagle.bark();
+
+// Understand the Immediately Invoked Function Expression (IIFE)
+
+(function () {
+  console.log("A cozy nest is ready");
+})();
