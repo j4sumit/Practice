a7b72c4560e226a2421d15cc98bd553f97981153
Sumit Kumar <j4sumit@gmail.com>
2023-06-10 23:12:36 +0530

    Flatten a nested array

--- leetcode.js
+++ leetcode.js
@@ -2679,18 +2679,33 @@
 //     }
 //   }
 // }
-
-// smallestCommons([1,5]);
+// console.log(smallestCommons([1,5]));
 
 
 // Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
 // Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
 
-function dropElements(arr, func) {
-  while(arr.length>0 && !func(arr[0]))
-  {
-    arr.shift();
+// function dropElements(arr, func) {
+//   while(arr.length>0 && !func(arr[0]))
+//   {
+//     arr.shift();
+//   }
+//   return arr;
+// }
+// console.log(dropElements([1, 2, 3], function(n) {return n < 3; }));
+
+
+// Flatten a nested array. You must account for varying levels of nesting.
+
+function steamrollArray(arr) {
+  const flattenedArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flattenedArray.push(...steamrollArray(arr[i]));
+    } else {
+      flattenedArray.push(arr[i]);
+    }
   }
-  return arr;
-}
-dropElements([1, 2, 3], function(n) {return n < 3; });
+  return flattenedArray;
+};
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
