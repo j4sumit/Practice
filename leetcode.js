7200a9ee483c5d4f0ec5584e0d7763700381cff7
Sumit Kumar <j4sumit@gmail.com>
2023-06-10 23:24:10 +0530

    binary to string

--- leetcode.js
+++ leetcode.js
@@ -2697,15 +2697,31 @@
 
 // Flatten a nested array. You must account for varying levels of nesting.
 
-function steamrollArray(arr) {
-  const flattenedArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i])) {
-      flattenedArray.push(...steamrollArray(arr[i]));
-    } else {
-      flattenedArray.push(arr[i]);
-    }
+// function steamrollArray(arr) {
+//   const flattenedArray = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (Array.isArray(arr[i])) {
+//       flattenedArray.push(...steamrollArray(arr[i]));
+//     } else {
+//       flattenedArray.push(arr[i]);
+//     }
+//   }
+//   return flattenedArray;
+// };
+// console.log(steamrollArray([1, [2], [3, [[4]]]]));
+
+function binaryAgent(str) {
+  var biString = str.split(" ");
+  var uniString = [];
+  for (var i = 0; i < biString.length; i++) {
+    // console.log(parseInt(biString[i], 2));
+    // console.log((String.fromCharCode(parseInt(biString[i], 2))));
+    uniString.push(String.fromCharCode(parseInt(biString[i], 2)));
   }
-  return flattenedArray;
-};
-console.log(steamrollArray([1, [2], [3, [[4]]]]));
+    return uniString.join("");
+}
+
+let result = binaryAgent(
+  "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
+);
+console.log(result);
