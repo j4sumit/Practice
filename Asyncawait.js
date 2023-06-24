b524bc0fb4a4dc630e1870623e912a26975baacb
Sumit Kumar <j4sumit@gmail.com>
2023-06-25 00:09:59 +0530

    Promise example

--- Asyncawait.js
+++ Asyncawait.js
@@ -1,9 +1,27 @@
-function SetTimeOut()
+// function SetTimeOut()
+// {
+//     console.log("line 1 will executed first");
+//     console.log("line 2 will executed second");
+//     setTimeout(()=>{console.log("Line 3 will executed last")},2000);
+//     console.log("line 4 will executed in 4th");
+//     console.log("line 5 will executed in 5th");
+// }
+// SetTimeOut();
+
+function chechnum(number)
 {
-    console.log("line 1 will executed first");
-    console.log("line 2 will executed second");
-    setTimeout(()=>{console.log("Line 3 will executed last")},2000);
-    console.log("line 4 will executed in 4th");
-    console.log("line 5 will executed in 5th");
+    let promise = new Promise((resolve, reject) =>{
+        let num=number;
+        if(num%2==0)
+        {
+            resolve("Num is Even");
+        }
+        else{
+            reject("Num is Odd");
+        }
+    });
+    promise.then((res)=>{console.log(res)}, (rej)=> {console.log(rej)});
+    return promise;
 }
-SetTimeOut();
+
+console.log(chechnum(24));
