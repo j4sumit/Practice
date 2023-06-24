834582a80f864ec4a4a98f7ef8a615c19ec0a7c0
Sumit Kumar <j4sumit@gmail.com>
2023-06-24 23:53:30 +0530

    setTimeout function

--- Asyncawait.js
+++ Asyncawait.js
@@ -1,46 +1,9 @@
-// //Simple Asyn function
-// function sample()
-// {
-//     console.log("first line");
-//     setTimeout(()=>{console.log("this line will be execute after seconde line"),1000})
-//     console.log("Senode line");
-// }
-
-// sample();
-// //Sample using Promise
-// function sample(num)
-// {
-//     let promise= new Promise((resolve, reject)=>{
-
-//         if(num%2==0)
-//         {
-//             resolve("number is even");
-//         }
-//         else{
-//             reject("number  is odd");
-//         }
-//     });
-//     promise.then((val)=> console.log(val),(val)=> console.log(val));
-    
-// }
-
-// sample(11);
-
-
-
-
-var promise= new Promise((resolve, reject)=>
+function SetTimeOut()
 {
-    let num=11;
-if(num%2==0)
-{
-resolve("num is even");
-} 
-else{
-reject("num is odd");
+    console.log("line 1 will executed first");
+    console.log("line 2 will executed second");
+    setTimeout(()=>{console.log("Line 3 will executed last")},2000);
+    console.log("line 4 will executed in 4th");
+    console.log("line 5 will executed in 5th");
 }
-});
-
-promise.then((result)=>{console.log(result)},(error)=>{console.log(error)});
-console.log(promise);
-
+SetTimeOut();
