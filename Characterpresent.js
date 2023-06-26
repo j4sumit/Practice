a9a32bc09b56eaac5ff348980faced9c261ced9c
Sumit Kumar <j4sumit@gmail.com>
2023-06-26 23:52:14 +0530

    Check Character present or not

--- Characterpresent.js
+++ Characterpresent.js
@@ -1,26 +1,15 @@
-function characterPresent(arr){
-    var first = arr[0].toLowerCase()
-    var second = arr[1].toLowerCase()
-    for (var letter of second){
-      if(first.indexOf(letter)== -1){
-        return false
-      }
-    }
-    return true
+function CheckChar(string, substr)
+{
+  let Str= string.toLowerCase();
+  let subst =substr.toLowerCase();
+  for(let char of subst)
+  {
+if(Str.indexOf(char)==-1)
+{
+  return false;
+}
   }
-  console.log(characterPresent(["hello","hel"]))
+ return true; 
+}
 
-// function Characterpresent(arr)
-// {
-// let first = arr[0].toLowerCase();
-// let second =arr[1].toLowerCase();
-// for(let str of second)
-// {
-//     if(!first.includes(str)){
-//         return false;
-//     }
-// }
-// return true;
-// }
-
-// console.log(Characterpresent(['hello', 'hel']));
+console.log(CheckChar("Sumit","sum"));
