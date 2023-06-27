ec0bef6947bf4020091b948635b7673952e0346e
Sumit Kumar <j4sumit@gmail.com>
2023-06-27 20:19:12 +0530

    Converting one dimmention to multidimension

--- Convertingonetomultidimension.js
+++ Convertingonetomultidimension.js
@@ -1,18 +1,11 @@
-// function chunkArrayInGroup(arr, size){
-//     var group=[]
-//     while(arr.length>0){
-//     group.push(arr.slice(0, size))
-//     arr = arr.slice(size)
-//     }
-//     console.log(group);
-//   }
-//   chunkArrayInGroup(['a','b','c','d'],2);
-
 function chunkArrayInGroup(arr, size){
-    var group=[]
+    var group=[];
+    let c;
     while(arr.length>0){
-    group.push(arr.splice(0, size))
+      c=arr.splice(0, size);
+      console.log(c)
+    group.push(c);
     }
     return group
   }
-  console.log (chunkArrayInGroup(['a','b','c','d','e','f'],3)) //[["a", "b"], ["c", "d"]]
+  console.log (chunkArrayInGroup(['a','b','c','d','e','f'],2)) //[["a", "b"], ["c", "d"]]
