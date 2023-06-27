fa84ad9bee41ae6f098ab54edd2a296bf34f7128
Sumit Kumar <j4sumit@gmail.com>
2023-06-27 20:29:21 +0530

    Converting one to multidimension using slice() method

--- Convertingonetomultidimension.js
+++ Convertingonetomultidimension.js
@@ -1,11 +1,26 @@
+// function chunkArrayInGroup(arr, size){
+//     var group=[];
+//     let c;
+//     while(arr.length>0){
+//       c=arr.splice(0, size);
+//       console.log(c)
+//     group.push(c);
+//     }
+
+//     return group
+//   }
+//   console.log (chunkArrayInGroup(['a','b','c','d','e','f'],2)) ;
+
 function chunkArrayInGroup(arr, size){
     var group=[];
-    let c;
+    
     while(arr.length>0){
-      c=arr.splice(0, size);
-      console.log(c)
-    group.push(c);
+      // console.log(arr);  
+    group.push(arr.slice(0, size))
+    arr = arr.slice(size)
+    
     }
-    return group
+    console.log(group);
+    
   }
-  console.log (chunkArrayInGroup(['a','b','c','d','e','f'],2)) //[["a", "b"], ["c", "d"]]
+  chunkArrayInGroup(['a','b','c','d'],2);
