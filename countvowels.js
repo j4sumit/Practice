0c026173e0f49e77193e163bf6a91d94a7468b47
Sumit Kumar <j4sumit@gmail.com>
2023-07-02 23:30:33 +0530

    count vowels from given string

--- countvowels.js
+++ countvowels.js
@@ -1,30 +1,10 @@
-// function vowels()
-// {
-//     let vowels = ['a','e','i','o','u'];
-//     let string = "sumitaaaUUUU";
-//     let count=0;
-
-// for (let i of string.toLowerCase())
-// {
-//     if (vowels.includes(i))
-//     {
-//         count++;
-//         console.log(i);
-        
-//     }
-// } 
-// return count;
-// }
-
-// console.log(vowels());
-
 function countv(str)
 {
  let vowels =['a','e','i','o','u'];
  let count=0;
  for(let i=0; i<str.length; i++)
  {
-     if(vowels.includes(str[i]))
+     if(vowels.includes(str[i].toLowerCase()))
      {
          count++;
      }
