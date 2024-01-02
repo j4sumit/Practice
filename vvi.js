9ec05ba5ca4790be952658e866505c93eab05120
Sumit Kumar <j4sumit@gmail.com>
2024-01-02 22:54:40 +0530

    ArmStrong Number

--- vvi.js
+++ vvi.js
@@ -1,4 +1,4 @@
-//Anagram String
+//1.    Anagram String
 function Anagram(str1, str2)
 {
 let s1=str1.toLowerCase().split("").sort().join("");
@@ -9,8 +9,27 @@ if(s1===s2)
 }else{
     return "String is not Anagram"
 }
-
-
 }
+// console.log(Anagram("Dad","awdd"));
 
-console.log(Anagram("Dad","awdd"));
+//2.    ArmStrong program
+
+function ArmStrong(num)
+{
+ let temp=0, newnum=num;
+while(newnum>0)
+{
+    let rem=newnum%10;
+    console.log(rem)
+    temp=temp + rem**3;
+    console.log(temp)
+    newnum= Math.floor(newnum/10);
+    console.log(newnum)
+}
+if(temp===num){
+    return "number is ArmSTrong"
+}else{
+    return "Number is not ArmStrong";
+}
+}
+console.log(ArmStrong(153));
